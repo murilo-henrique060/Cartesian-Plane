f8dda4bde75e13f928b68dd33b24a06275688dd1
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2022-07-29 06:54:18 -0300

    add a focus function

--- src/cartesianPlane/__init__.py
+++ src/cartesianPlane/__init__.py
@@ -7,22 +7,33 @@ from cartesianPlane.colors import *
 __version__ = "1.0.1"
 
 class CartesianPlane:
-    def __init__(self, screen):
+    def __init__(self, size : list | tuple, caption : str = "Cartesian Plane"):
         """Initialize the Cartesian Plane
 
         Args:
             screen (pygame.display): The screen to draw on
         """
-        self.screen = screen
-        self.size = screen.get_size()
-        self.rect = self.screen.get_rect()
-        self.offset = list(self.rect.center)
+        # Initialize the pygame
+        pygame.init()
+
+        # Create the screen
+        self._size = [*size]
+        self.screen = pygame.display.set_mode(self._size, flags=RESIZABLE)
+        pygame.display.set_caption(caption)
+
+        self.offset = [
+            self._size[0] // 2,
+            self._size[1] // 2
+        ]
 
         self.objects = []
 
+        # Blocking movement of the plane
+        self.block_movement = False
+
         # track mouse position
-        self.last_pos = None
-        self.move_plane = False
+        self._last_pos = None
+        self._move_plane = False
 
         self.scale_factor = 1
 
@@ -32,7 +43,6 @@ class CartesianPlane:
 
         # Debug Info
         self.debug_info = {}
-
 
     def plane_to_screen(self, point : list | tuple) -> list:
         """Convert Cartesian Coordinates to Screen Coordinates
@@ -71,32 +81,48 @@ class CartesianPlane:
         """
         return round(dimention * self.scale_factor)
 
-    def move_axis(self) -> None:
+    def centralize(self, point : list | tuple) -> None:
+        """Centralize a point of the Cartesian Plane
+        """
+        point = self.plane_to_screen(point)
+        diff = [
+            self._size[0] // 2 - point[0],
+            self._size[1] // 2 - point[1]
+        ]
+
+        self.offset[0] += diff[0] / self.scale_factor
+        self.offset[1] += diff[1] / self.scale_factor
+
+    def _move_axis(self) -> None:
         """Move the axis of the Cartesian Plane
         """
-        current_pos = pygame.mouse.get_pos()
-        if self.last_pos:
-            self.offset[0] += (current_pos[0] - self.last_pos[0]) / self.scale_factor
-            self.offset[1] += (current_pos[1] - self.last_pos[1]) / self.scale_factor
+        if not self.block_movement:
+            current_pos = pygame.mouse.get_pos()
+            if self._last_pos:
+                self.offset[0] += (current_pos[0] - self._last_pos[0]) / self.scale_factor
+                self.offset[1] += (current_pos[1] - self._last_pos[1]) / self.scale_factor
 
-        self.last_pos = current_pos
+            self._last_pos = current_pos
 
-    def zooming(self, wheel : int) -> None:
+    def _zooming(self, wheel : int) -> None:
         """Zoom the Cartesian Plane
 
         Args:
             wheel (int): The amount of wheel movement
         """
-        before_zooming = self.screen_to_plane(pygame.mouse.get_pos())
+        mouse_pos = pygame.mouse.get_pos()
+        before_zooming = self.screen_to_plane(mouse_pos)
         if wheel > 0 and self.scale_factor < 100:
             self.scale_factor *= 1.1
 
         elif wheel < 0 and self.scale_factor > 0.1:
             self.scale_factor *= 0.9
 
-        after_zooming = self.screen_to_plane(pygame.mouse.get_pos())
-        self.offset[0] -= before_zooming[0] - after_zooming[0]
-        self.offset[1] += before_zooming[1] - after_zooming[1]
+        after_zooming = self.screen_to_plane(mouse_pos)
+
+        if not self.block_movement:
+            self.offset[0] -= before_zooming[0] - after_zooming[0]
+            self.offset[1] += before_zooming[1] - after_zooming[1]
 
     def debug(self, **kwargs) -> None:
         """Add debug information to the Cartesian Plane
@@ -104,42 +130,49 @@ class CartesianPlane:
         for key, value in kwargs.items():
             self.debug_info[key] = value
 
-        offset = 5
-        for key, value in self.debug_info.items():
-            text = self.font.render(f'{key}: {value}', True, WHITE)
-            self.screen.blit(text, (10, offset))
-            offset += text.get_height()
-
-    def event_handling(self, event) -> None:
+    def get_events(self, internal_events : bool = True) -> list:
         """Handle events
 
         Args:
             event (pygame.locals): The event to handle
         """
-        if event.type == MOUSEBUTTONDOWN and event.button == 1:
-            self.move_plane = True
+        events = pygame.event.get()
 
-        if event.type == MOUSEBUTTONUP and event.button == 1:
-            self.move_plane = False
-            self.last_pos = None
+        if internal_events:
+            for event in events:
+                if event.type == QUIT:
+                    pygame.quit()
+                    quit()
 
-        if event.type == MOUSEWHEEL:
-            self.zooming(event.y)
+                if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    self._move_plane = True
 
+                if event.type == MOUSEBUTTONUP and event.button == 1:
+                    self._move_plane = False
+                    self._last_pos = None
+
+                if event.type == MOUSEWHEEL:
+                    self._zooming(event.y)
+
+                if event.type == WINDOWSIZECHANGED:
+                    self._size[0] = event.x
+                    self._size[1] = event.y
+
+        return events
 
     def update(self):
         """Update the Cartesian Plane
         """
-        if self.move_plane:
-            self.move_axis()
+        if self._move_plane:
+            self._move_axis()
 
         # Clear the screen
         self.screen.fill(BLACK)
 
         # Draw the axis of Cartesian Plane
         origin = self.plane_to_screen((0, 0))
-        pygame.draw.line(self.screen, WHITE, (0, origin[1]), (self.size[0], origin[1]), 1) # X-Axis = (0, origin_y) to (screen_width, origin_y)
-        pygame.draw.line(self.screen, WHITE, (origin[0], 0), (origin[0], self.size[1]), 1) # Y-Axis = (origin_x, 0) to (origin_x, screen_height)
+        pygame.draw.line(self.screen, WHITE, (0, origin[1]), (self._size[0], origin[1]), 1) # X-Axis = (0, origin_y) to (screen_width, origin_y)
+        pygame.draw.line(self.screen, WHITE, (origin[0], 0), (origin[0], self._size[1]), 1) # Y-Axis = (origin_x, 0) to (origin_x, screen_height)
 
         # Draw the objects
         for object in self.objects:
@@ -154,3 +187,18 @@ class CartesianPlane:
             # Plane_Pos=self.screen_to_plane(pygame.mouse.get_pos()),
             # Converted_Mouse_Pos=self.plane_to_screen(self.screen_to_plane(pygame.mouse.get_pos()))
         )
+
+        offset = 5
+        for key, value in self.debug_info.items():
+            text = self.font.render(f'{key}: {value}', True, WHITE)
+            self.screen.blit(text, (10, offset))
+            offset += text.get_height()
+
+        pygame.display.flip()
+
+plane = CartesianPlane((800, 600))
+
+while True:
+    plane.get_events()
+
+    plane.update()
